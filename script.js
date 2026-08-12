ffbf3eb9df4b9b2a095077b5d69cdb5b3c4f9224
Sherly2000 <sherdlyvr@gmail.com>
2026-08-11 20:25:44 -0400

    Add the display function to display the books in a table

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const myLibrary = [];
 
-function Book (id,title, author, pages, type) {
+function Book (title, author, pages, type) {
     this.id = crypto.randomUUID();
     this.title = title;
     this.author = author;
@@ -10,11 +10,76 @@ function Book (id,title, author, pages, type) {
 
 
 function addBooksToLibrary (){
-const book1 = new Book("id", "Le Corbeau", "Jean de la Fontaine", 40, "Fables");
+const book1 = new Book( "Le Corbeau", "Jean de la Fontaine", 40, "Fables");
 
- return myLibrary.push(book1) ;
+const book2 = new Book( "L'Histoire de 1804", "Vernain Shella", 350, "Histoty");
+
+const book3 = new Book("Passion & Power", "Esther de la Rose", 200, "Roman d'amour");
+
+myLibrary.push(book1) ;
+myLibrary.push(book2);
+myLibrary.push(book3);
+
+return myLibrary;
 }
 
 console.log(addBooksToLibrary());
 
-console.log(myLibrary);
+
+
+// add a button
+
+const container = document.querySelector(".container");
+
+const button = document.createElement('button');
+
+button.style.color = "green";
+button.textContent = "Books";
+
+container.appendChild(button);
+
+
+// add the function display
+
+function displayBook (){
+
+const table = document.querySelector("#library-body");
+
+table.innerHTML = "";
+
+  myLibrary.forEach(book => {
+    
+    const row = document.createElement("tr");
+
+     const tdId = document.createElement("td");
+    tdId.classList.add("data");
+    tdId.textContent = book.id ;
+    row.appendChild(tdId);
+
+     const tdTitle = document.createElement("td");
+    tdTitle.classList.add("data");
+    tdTitle.textContent = book.title ;
+    row.appendChild(tdTitle);
+
+     const tdAuthor = document.createElement("td");
+    tdAuthor.classList.add("data");
+    tdAuthor.textContent = book.author ;
+    row.appendChild(tdAuthor);
+
+     const tdPages = document.createElement("td");
+    tdPages.classList.add("data");
+    tdPages.textContent = book.pages || 0;
+    row.appendChild(tdPages);
+
+    const tdType = document.createElement("td");
+    tdType.classList.add("data");
+    tdType.textContent = book.type ;
+    row.appendChild(tdType);
+
+     table.appendChild(row);
+
+})};
+
+button.addEventListener("click", ()  =>  {
+    displayBook();
+});
